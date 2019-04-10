c8b7ffc8513469d4b9dec5683606464f377ac4ca
David Endrew <davideas000@gmail.com>
2019-04-10 16:47:45 -0300

    ft: add user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+export class RaUser {
+  _id: string;
+  name: string;
+  role: string;
+  displayName?: string;
+  email: string;
+  photoURL?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
