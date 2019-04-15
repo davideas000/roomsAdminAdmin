a0eff4ff4b4b35082117d8deb334e70aad35babc
David Endrew <davideas000@gmail.com>
2019-04-14 22:19:03 -0300

    ft(Login): add a route to the login page

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RaLoginComponent } from './login.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: RaLoginComponent}
 ];
 
 @NgModule({
