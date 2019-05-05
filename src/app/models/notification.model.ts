130c3e93a50b4d0b33354ba1ac1af5d4df4c2b43
David Endrew <davideas000@gmail.com>
2019-05-05 20:39:42 -0300

    refac(model): update notification model

--- src/app/models/notification.model.ts
+++ src/app/models/notification.model.ts
@@ -1,7 +1,12 @@
+import { RaReservation } from "./reservation.model";
+
 export class RaNotification {
   _id: string;
   message: string;
+  extra?: string;
   status: string;
+  category: string;
+  reservation?: RaReservation;
   createdAt?: Date;
   updatdAt?: Date;
 }
