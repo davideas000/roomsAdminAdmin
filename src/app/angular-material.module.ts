36effb25e2eaed863d478282cbe937868e515ec7
David Endrew <davideas000@gmail.com>
2019-05-19 12:22:18 -0300

    style: put the imports in column format

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,6 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatFormFieldModule, MatInputModule, MatSidenavModule, MatButtonModule, MatSlideToggleModule, MatCardModule, MatProgressSpinnerModule, MatIconModule, MatListModule, MatRippleModule, MatDatepickerModule } from '@angular/material';
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatSlideToggleModule,
+  MatCardModule,
+  MatProgressSpinnerModule,
+  MatIconModule,
+  MatListModule,
+  MatRippleModule,
+  MatDatepickerModule
+} from '@angular/material';
 import { PortalModule } from '@angular/cdk/portal';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
