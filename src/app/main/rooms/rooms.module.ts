940b77e702171b991d0c84bc62a5178eb0875ada
David Endrew <davideas000@gmail.com>
2019-05-23 19:35:12 -0300

    feat(RoomsList): add module to contain elements related to rooms listing

--- src/app/main/rooms/rooms.module.ts
+++ src/app/main/rooms/rooms.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RaAngularMaterialModule } from 'src/app/angular-material.module';
+import { RaRoomsListModule } from './rooms-list/rooms-list.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     RaAngularMaterialModule,
+    RaRoomsListModule
+  ],
+  exports: [
+    RaRoomsListModule
   ]
 })
 export class RaRoomsModule {}
