2e4ad98bce465efb0e45c5f6fbf3d6edc56e6db1
David Endrew <davideas000@gmail.com>
2019-05-24 15:03:56 -0300

    feat(ReservationsList): add module to contain elements related to reservations listing

--- src/app/main/reservations/reservations.module.ts
+++ src/app/main/reservations/reservations.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RaReservationsListModule } from './reservations-list/reservations-list.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    RaReservationsListModule
   ],
   exports: [
+    RaReservationsListModule
   ]
 })
 export class RaReservationsModule { }
