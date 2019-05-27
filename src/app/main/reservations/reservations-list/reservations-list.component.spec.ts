17110d325b387ee9fe9ef0f9236f78aab4837a7d
David Endrew <davideas000@gmail.com>
2019-05-27 14:51:41 -0300

    feat(ReservationsList); add remove() method
    add a method to remove a reservation that raises a remove event

--- src/app/main/reservations/reservations-list/reservations-list.component.spec.ts
+++ src/app/main/reservations/reservations-list/reservations-list.component.spec.ts
@@ -2,15 +2,32 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RaReservationsListComponent } from './reservations-list.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RaApiService } from 'src/app/api.service';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { of, throwError } from 'rxjs';
+import { RaConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
+import { RaOperationResultNotificationComponent } from './operation-result-notification/operation-result-notification.component';
 
 describe('RaReservationsListComponent', () => {
   let component: RaReservationsListComponent;
   let fixture: ComponentFixture<RaReservationsListComponent>;
 
   beforeEach(async(() => {
+
+    let apiSpy = jasmine.createSpyObj('RaApiService',
+                                      ['removeReservation$']);
+    let dialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
+    let snackBarSpy = jasmine.createSpyObj('MatSnackBar',
+                                           ['openFromComponent']);
+
     TestBed.configureTestingModule({
       declarations: [ RaReservationsListComponent ],
-      schemas: [ NO_ERRORS_SCHEMA ]
+      schemas: [ NO_ERRORS_SCHEMA ],
+      providers: [
+        {provide: RaApiService, useValue: apiSpy},
+        {provide: MatDialog, useValue: dialogSpy},
+        {provide: MatSnackBar, useValue: snackBarSpy}
+      ]
     }).compileComponents();
   }));
 
@@ -48,5 +65,105 @@ describe('RaReservationsListComponent', () => {
 
     expect(reservs.length).toBe(reservationsStub.length);
   });
+
+  it('should remove the reservation (`<ra-reservation>`) that raises '
+     + 'a remove event', () => {
+       const reservationsStub: any = [
+         {_id: 'reserv01'}, {_id: 'reserv02'}
+       ];
+       component.reservations = reservationsStub;
+       fixture.detectChanges();
+       const reservation: HTMLElement = fixture.nativeElement
+         .querySelector('ra-reservation');
+       expect(reservation).toBeTruthy();
+
+       const dialogSpy = fixture.debugElement.injector
+         .get(MatDialog) as jasmine.SpyObj<MatDialog>;
+
+       const dialogRefMock = {afterClosed: () => of(true)};
+       dialogSpy.open.and.returnValue(dialogRefMock);
+
+       const apiSpy = fixture.debugElement.injector
+         .get(RaApiService) as jasmine.SpyObj<RaApiService>;
+       apiSpy.removeReservation$.and.returnValue(of(true));
+
+       const snackSpy = fixture.debugElement.injector
+         .get(MatSnackBar) as jasmine.SpyObj<MatSnackBar>;
+
+       reservation.dispatchEvent(new Event('remove'));
+       fixture.detectChanges();
+
+       expect(dialogSpy.open).toHaveBeenCalledTimes(1);
+       expect(dialogSpy.open).toHaveBeenCalledWith(
+         RaConfirmationDialogComponent, {data: {type: 'remove',
+                                                showMessage: true,
+                                                showTextarea: false}});
+       expect(apiSpy.removeReservation$).toHaveBeenCalledTimes(1);
+       expect(apiSpy.removeReservation$)
+         .toHaveBeenCalledWith(reservationsStub[0]);
+       expect(snackSpy.openFromComponent).toHaveBeenCalledTimes(1);
+
+       expect(snackSpy.openFromComponent).toHaveBeenCalledWith(
+         RaOperationResultNotificationComponent,
+         {
+           data: {type: 'removed'},
+           duration: 2000
+         }
+       );
+
+       let result = false;
+       component.reservations.forEach(
+         reservation => {
+           if(reservation._id === reservationsStub[0]._id)
+           { result = true}
+         }
+       );
+       expect(result).toBe(false);
+
+       const reservsEl: NodeList = fixture.nativeElement
+         .querySelectorAll('ra-reservation');
+       expect(reservsEl.length).toBe(1);
+     });
+
+  it('should display a snackbar with an error message when the removal of '
+     + 'a reservation fails', () => {
+
+       const reservationsStub: any = [
+         {_id: 'reservA'}, {_id: 'reservB'}
+       ];
+
+       const dialogSpy = fixture.debugElement.injector
+         .get(MatDialog) as jasmine.SpyObj<MatDialog>;
+
+       const dialogRefMock = {afterClosed: () => of(true)};
+       dialogSpy.open.and.returnValue(dialogRefMock);
+
+       const apiSpy = fixture.debugElement.injector
+         .get(RaApiService) as jasmine.SpyObj<RaApiService>;
+       // fails with a error
+       apiSpy.removeReservation$.and.returnValue(throwError('error'));
+
+       const snackSpy = fixture.debugElement.injector
+         .get(MatSnackBar) as jasmine.SpyObj<MatSnackBar>;
+
+       component.remove(reservationsStub[0]);
+
+       expect(dialogSpy.open).toHaveBeenCalledTimes(1);
+       expect(dialogSpy.open).toHaveBeenCalledWith(
+         RaConfirmationDialogComponent, {data: {type: 'remove',
+                                                showMessage: true,
+                                                showTextarea: false}});
+
+       expect(apiSpy.removeReservation$).toHaveBeenCalledTimes(1);
+       expect(apiSpy.removeReservation$)
+         .toHaveBeenCalledWith(reservationsStub[0]);
+
+       expect(snackSpy.openFromComponent).toHaveBeenCalledTimes(1);
+       expect(snackSpy.openFromComponent).toHaveBeenCalledWith(
+         RaOperationResultNotificationComponent,
+         {data: {type: 'error'}}
+       );
+
+     });
 
 });
--- src/app/main/reservations/reservations-list/reservations-list.component.ts
+++ src/app/main/reservations/reservations-list/reservations-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { RaApiService } from 'src/app/api.service';
+import { RaConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { first } from 'rxjs/operators';
 import { RaReservation } from 'src/app/models/reservation.model';
+import { RaOperationResultNotificationComponent } from './operation-result-notification/operation-result-notification.component';
 
 @Component({
   selector: 'ra-reservations-list',
@@ -12,4 +17,42 @@ export class RaReservationsListComponent {
   @Input() showTitle = true;
   @Input() showActions = true;
   @Input() showStatus = false;
+
+  constructor(private apiService: RaApiService,
+              private dialog: MatDialog,
+              private snackBar: MatSnackBar) {}
+
+  remove(reservation: RaReservation) {
+    const dialogRef = this.dialog.open(
+      RaConfirmationDialogComponent,
+      {data: {type: 'remove',
+              showMessage: true,
+              showTextarea: false}});
+
+    dialogRef.afterClosed()
+      .subscribe(r => {
+        if(r) {
+          this.apiService.removeReservation$(reservation)
+            .pipe(first())
+            .subscribe(
+              _ => {
+                this.reservations = this.reservations.filter(
+                  v => v._id !== reservation._id
+                );
+                this.openSnackbar('removed');
+              },
+              _ => {
+                this.openSnackbar('error');
+              });
+        }
+      });
+  }
+
+  private openSnackbar(mtype: string) {
+    let config: any = {data: {type: mtype}};
+    if(mtype !== 'error') config.duration = 2000
+    this.snackBar.openFromComponent(
+      RaOperationResultNotificationComponent, config);
+  }
+
 }
