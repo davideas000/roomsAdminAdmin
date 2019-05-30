e350337876eef53c094e5ddb1516e61c1cd8d996
David Endrew <davideas000@gmail.com>
2019-05-30 11:29:57 -0300

    feat(Main): add module routes

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RaMainComponent } from './main.component';
+import { roomsPagesRoutes } from './rooms-pages/rooms-pages-routes';
+import { newReservationRoutes } from './new-reservation/new-reservation-routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    component: RaMainComponent,
+    children: [
+      roomsPagesRoutes,
+      newReservationRoutes
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RaMainComponent } from './main.component';
 import { RaMainRoutingModule } from './main-routing.module';
+import { RaRoomsPagesModule } from './rooms-pages/rooms-pages.module';
+import { RaNewReservationModule } from './new-reservation/new-reservation.module';
 
 @NgModule({
   declarations: [RaMainComponent],
   imports: [
     CommonModule,
-    RaMainRoutingModule
+    RaMainRoutingModule,
+    RaRoomsPagesModule,
+    RaNewReservationModule
   ]
 })
 export class RaMainModule { }
