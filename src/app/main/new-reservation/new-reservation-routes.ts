f2811ce95b4d88113791a72944d670da3e5829a6
David Endrew <davideas000@gmail.com>
2019-05-30 11:29:12 -0300

    feat(NewReservation): add module routes

---
+++ src/app/main/new-reservation/new-reservation-routes.ts
@@ -0,0 +1,7 @@
+import { Route } from '@angular/router';
+import { RaNewReservationComponent } from './new-reservation.component';
+
+
+export const newReservationRoutes: Route = {
+  path: 'new-reservation', component: RaNewReservationComponent
+}
