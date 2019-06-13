638ceb64e8e801647b1ce185ea9325d2e1cfb782
David Endrew <davideas000@gmail.com>
2019-06-12 21:33:43 -0300

    refac(UserMenu): remove unused imports

--- src/app/main/header/user-menu/user-menu.component.ts
+++ src/app/main/header/user-menu/user-menu.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RaAuthService } from 'src/app/auth/auth.service';
 import { RaUser } from 'src/app/models/user.model';
-import { Overlay, OverlayRef, CdkOverlayOrigin } from '@angular/cdk/overlay';
-import { CdkPortal } from '@angular/cdk/portal';
 
 @Component({
   selector: 'ra-user-menu',
