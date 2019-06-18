10ee8c7bd1447389ecbde6bed9ceebd9d7494f1d
David Endrew <davideas000@gmail.com>
2019-06-18 20:53:11 -0300

    refact(RoomsModule): remove unused import

--- src/app/main/rooms/rooms.module.ts
+++ src/app/main/rooms/rooms.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RaAngularMaterialModule } from 'src/app/angular-material.module';
 import { RaRoomsListModule } from './rooms-list/rooms-list.module';
 import { RaRoomsSearchModule } from './rooms-search/rooms-search.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    RaAngularMaterialModule,
     RaRoomsListModule,
     RaRoomsSearchModule
   ],
