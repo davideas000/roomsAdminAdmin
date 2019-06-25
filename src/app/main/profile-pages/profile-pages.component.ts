ffca949c1c7b37f46a3819f81b7875370de5b557
David Endrew <davideas000@gmail.com>
2019-06-25 13:51:30 -0300

    feat(ProfilePages): add comp. that groups pages related to profile
    this comp. only contains a router outlet

---
+++ src/app/main/profile-pages/profile-pages.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ra-profile-pages',
+  template: '<router-outlet></router-outlet>'
+})
+export class RaProfilePagesComponent {}
--- src/app/main/profile-pages/profile-pages.module.ts
+++ src/app/main/profile-pages/profile-pages.module.ts
@@ -4,11 +4,13 @@ import { RaAngularMaterialModule } from 'src/app/angular-material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { RaProfilePagesComponent } from './profile-pages.component';
 import { RaProfileComponent } from './profile/profile.component';
 import { RaEditProfileComponent, RaUpdateProfileSucessComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
   declarations: [
+    RaProfilePagesComponent,
     RaProfileComponent,
     RaEditProfileComponent,
     RaUpdateProfileSucessComponent
